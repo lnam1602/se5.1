65d9a728af0a9db4454bd6d3b44b224891cdb528
luongmaiquynh <luongmaiquynh_t66@hus.edu.vn>
2024-01-03 01:49:59 +0700

    Fix lỗi fence_default bị lộ

---
+++ home-decor-client/Assets/Scripts/ReplayButton.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Data;
+using UnityEngine;
+using Mono.Data.Sqlite;
+using System.Collections.Generic;
+using System.Collections;
+
+
+public class ReplayButton : MonoBehaviour
+{
+    public void onReplay()
+    {
+        Debug.Log("On Replay");
+        IDbConnection dbConnectionRoom = CreateAndOpenDatabase("RoomCurrent");
+        IDbCommand dbCommandCheckRoom = dbConnectionRoom.CreateCommand();
+        dbCommandCheckRoom.CommandText = $"SELECT object_name FROM RoomCurrent WHERE id = 1";
+        object res = dbCommandCheckRoom.ExecuteScalar();
+        string room = Convert.ToString(res);
+        var furnitures = new List<GameObject>();
+        // Read all values from the table.
+        IDbConnection dbConnection = CreateAndOpenDatabase(room);
+        IDbCommand dbCommandReadValues = dbConnection.CreateCommand();
+        dbCommandReadValues.CommandText = $@"SELECT * FROM {room}";
+        IDataReader dataReader = dbCommandReadValues.ExecuteReader();
+
+        if (room == "Veranda")
+        {
+            fenceDefault.SetActive(true);
+        }
+
+        while (dataReader.Read())
+        {
+            var objectName = dataReader.GetString(1);
+            GameObject obj = GameObject.Find(objectName);
+            furnitures.Add(obj);
+        }
+
+        foreach (var obj in furnitures)
+        {
+            obj.SetActive(false);
+        }
+
+        dbConnectionRoom.Close();
+        dbConnection.Close();
+        // Show all furnitures again
+        StartCoroutine(ShowObjectsWithDelay(furnitures, room));
+    }
+
+    private IEnumerator ShowObjectsWithDelay(List<GameObject> gameObjects, string room)
+    {
+
+        yield return new WaitForSeconds(1f); // Đổi giá trị độ trễ tại đây
+
+        foreach (var obj in gameObjects)
+        {
+            yield return new WaitForSeconds(0.5f);
+            if (obj.name.Split("_")[0] == "fence" && room == "Veranda")
+            {
+                fenceDefault.SetActive(false);
+            }
+            obj.SetActive(true);
+        }
+    }
+
+    private IDbConnection CreateAndOpenDatabase(string tableName)
+    {
+        // Open a connection to the database.
+        string dbUri = "URI=file:MyDatabase.sqlite";
+        IDbConnection dbConnection = new SqliteConnection(dbUri);
+        dbConnection.Open();
+
+        // Create a table for the hit count in the database if it does not exist yet.
+        IDbCommand dbCommandCreateTable = dbConnection.CreateCommand();
+        dbCommandCreateTable.CommandText = $@"
+            CREATE TABLE IF NOT EXISTS {tableName} (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                object_name TEXT NOT NULL
+            )
+        ";
+        dbCommandCreateTable.ExecuteReader();
+
+        return dbConnection;
+    }
+
+    public GameObject fenceDefault;
+}
+
